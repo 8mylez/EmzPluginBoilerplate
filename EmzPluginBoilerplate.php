990b0aaca8990607f1025d3fe66ab01d72b43896
Alexander Wolf <alexander@goltfisch.de>
2019-04-01 22:27:59 +0200

    Add clear cache stuff to install, uninstall, etc. methods

--- EmzPluginBoilerplate.php
+++ EmzPluginBoilerplate.php
@@ -3,8 +3,51 @@
 namespace EmzPluginBoilerplate;
 
 use Shopware\Components\Plugin;
+use Shopware\Components\Plugin\Context\ActivateContext;
+use Shopware\Components\Plugin\Context\DeactivateContext;
+use Shopware\Components\Plugin\Context\InstallContext;
+use Shopware\Components\Plugin\Context\UninstallContext;
+use Shopware\Components\Plugin\Context\UpdateContext;
 
 class EmzPluginBoilerplate extends Plugin
 {
+    /**
+     * @param InstallContext $context
+     */
+    public function install(InstallContext $context)
+    {
+        $context->scheduleClearCache(InstallContext::CACHE_LIST_ALL);
+    }
 
+    /**
+     * @param UpdateContext $context
+     */
+    public function update(UpdateContext $context)
+    {
+        $context->scheduleClearCache(UpdateContext::CACHE_LIST_ALL);
+    }
+
+    /**
+     * @param ActivateContext $context
+     */
+    public function activate(ActivateContext $context)
+    {
+        $context->scheduleClearCache(ActivateContext::CACHE_LIST_ALL);
+    }
+
+    /**
+     * @param DeactivateContext $context
+     */
+    public function deactivate(DeactivateContext $context)
+    {
+        $context->scheduleClearCache(DeactivateContext::CACHE_LIST_ALL);
+    }
+
+    /**
+     * @param UninstallContext $context
+     */
+    public function uninstall(UninstallContext $context)
+    {
+        $context->scheduleClearCache(UninstallContext::CACHE_LIST_ALL);
+    }
 }
